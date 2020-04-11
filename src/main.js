8340a80c955ebd5a27ef207bcc8014541302630a
Brian <brpereir@ucsc.edu>
2020-04-11 16:12:05 -0700

    finished rocket patrol

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 let config = {
-    type: Phaser.CANVAS, 
+    type: Phaser.CANVAS,
     width: 640,
     height: 480,
     scene: [Menu, Play],  // order is important 
@@ -10,3 +10,10 @@ let game = new Phaser.Game(config);
 // reserve some keyboard variables
 
 let keyF, keyLEFT, keyRIGHT;
+
+// define game settings
+
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,39 +1,43 @@
 // Rocket prefab
 
 class Rocket extends Phaser.GameObjects.Sprite {    // use phasers sprite
-    constructor(scene, x, y, texture, frame){
-
+    constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-
-        
         scene.add.existing(this); // add object to existing scene, built into phaser
-            this.isFiring = false; // tracking rocket's firing status
+        this.isFiring = false; // tracking rocket's firing status
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // adding rocket sfx
 
 
 
     }
 
-    update(){
-        if(!this.isFiring){   // left / right movement
-            if(keyLEFT.isDown && this.x >= 47){
+    update() {
+        if (!this.isFiring) {   // left / right movement
+            if (keyLEFT.isDown && this.x >= 47) {
                 this.x -= 2;
-            } else if (keyRIGHT.isDown && this.x <= 592){
+            } else if (keyRIGHT.isDown && this.x <= 592) {
                 this.x += 2;
             }
         }
 
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play(); // play sfx
         }
 
-        if(this.isFiring && this.y >= 108){
+        if (this.isFiring && this.y >= 108) {
             this.y -= 2;
         }
 
-        if(this.y <= 108){
-            this.isFiring = false;
-            this.y = 431;
+        if (this.y <= 108) {
+            this.reset();
         }
+
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
 
     }
 
